a128b4cd7c25be24b4e756e8b435c332f87fae74
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2023-02-05 17:55:16 +0100

    adding content to recipes component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is a description',
+      'https://images.ctfassets.net/' +
+      '314o13npeir2/BSe9tS2H4ayHBWmf58S7G/' +
+      '322852b8d00f3075addd563bf554cb52/' +
+      'kemences-pizza-budapest-14.jpg?w=1200&fl=progressive&q=80')
+  ];
 }
